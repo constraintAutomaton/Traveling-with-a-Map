2a186e719aeeb5e86e4b10816c4c4918b4787729
Bryan-Elliott Tam <bryanelliott.tam@ugent.be>
2025-09-30 08:22:53 +0200

    small addition

---
+++ clone_repos_supplementary_material.sh
@@ -0,0 +1,18 @@
+pushd repos
+    git clone https://github.com/shapeIndexComunicaExperiment/analysis.git &
+    git clone https://github.com/shapeIndexComunicaExperiment/source-selection-oracle.git &
+    git clone https://github.com/shapeIndexComunicaExperiment/shape-minimal-description-experiment.git &
+    git clone https://github.com/shapeIndexComunicaExperiment/shape-inner-dataset-experiment.git &
+    git clone https://github.com/shapeIndexComunicaExperiment/shape-index-80-percent-entries.git &
+    git clone https://github.com/shapeIndexComunicaExperiment/shape-index-80-percent-dataset.git &
+    git clone https://github.com/shapeIndexComunicaExperiment/shape-index-50-percent-entries.git &
+    git clone https://github.com/shapeIndexComunicaExperiment/shape-index-50-percent-dataset.git &
+    git clone https://github.com/shapeIndexComunicaExperiment/shape-index-20-percent-entries.git &
+    git clone https://github.com/shapeIndexComunicaExperiment/shape-index-20-percent-dataset.git &
+    git clone https://github.com/shapeIndexComunicaExperiment/shape-index-0-percent-dataset.git &
+    git clone https://github.com/shapeIndexComunicaExperiment/standard-experiment.git &
+    git clone https://github.com/shapeIndexComunicaExperiment/documentation.git &
+    git clone https://github.com/shapeIndexComunicaExperiment/.github.git &
+    git clone https://github.com/shapeIndexComunicaExperiment/results.git &
+    wait
+popd
